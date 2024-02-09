6cc62dcb0f78301b90dea06bd91cd0a201144571
ludovicroland <ludovic.roland@decathlon.com>
2024-02-10 00:26:37 +0100

    chore(kotlin): migrate the DetailsFragment to kotlin

--- app/src/main/java/com/animals/safety/fragments/DetailsFragment.kt
+++ app/src/main/java/com/animals/safety/fragments/DetailsFragment.kt
@@ -1,48 +1,48 @@
-package com.animals.safety.fragments;
+package com.animals.safety.fragments
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.animals.safety.data.Animal
+import com.animals.safety.databinding.FragmentDetailsBinding
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import com.animals.safety.data.Animal;
-import com.animals.safety.databinding.FragmentDetailsBinding;
-
-public class DetailsFragment extends Fragment {
-
-    public static final String KEY_ANIMAL = "ANIMAL";
-
-    private FragmentDetailsBinding binding;
-
-    private Animal getAnimal() {
-        return (Animal) getArguments().getSerializable(KEY_ANIMAL);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentDetailsBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        /*
-        binding.imageViewAvatar.setImageDrawable(AppCompatResources.getDrawable(getContext(), getAnimal().getBreed().getCover()));
-        binding.textViewName.setText(getAnimal().getName());
-        binding.textViewAge.setText(getString(R.string.value_age, String.valueOf(getAnimal().getAge())));
-        binding.textViewWeight.setText(getString(R.string.value_weight, String.valueOf(getAnimal().getWeight())));
-        binding.textViewHeight.setText(getString(R.string.value_height, String.valueOf(getAnimal().getHeight())));
-        */
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
+class DetailsFragment : Fragment() {
+  
+  private var binding: FragmentDetailsBinding? = null
+  
+  private val animal: Animal?
+    get() = arguments?.getSerializable(KEY_ANIMAL) as Animal?
+  
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    binding = FragmentDetailsBinding.inflate(inflater, container, false)
+    return binding?.root
+  }
+  
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    
+    //TODO
+//    binding.imageViewAvatar.setImageDrawable(AppCompatResources.getDrawable(getContext(), getAnimal().getBreed().getCover()));
+//    binding.textViewName.setText(getAnimal().getName());
+//    binding.textViewAge.setText(getString(R.string.value_age, String.valueOf(getAnimal().getAge())));
+//    binding.textViewWeight.setText(getString(R.string.value_weight, String.valueOf(getAnimal().getWeight())));
+//    binding.textViewHeight.setText(getString(R.string.value_height, String.valueOf(getAnimal().getHeight())));
+  
+  }
+  
+  override fun onDestroyView() {
+    super.onDestroyView()
+    binding = null
+  }
+  
+  companion object {
+    const val KEY_ANIMAL = "ANIMAL"
+  }
+  
 }
