0b42eecc897fb0fa7b1d48718715b4b2eeea156a
ludovicroland <ludovic.roland@decathlon.com>
2024-02-10 00:07:00 +0100

    chore(gradle): update gradle, the compileSDK and the targetSDK to latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
   namespace = "com.animals.safety"
-  compileSdk = 33
+  compileSdk = 34
   
   defaultConfig {
     applicationId = "com.animals.safety"
     minSdk = 24
-    targetSdk = 33
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
     
