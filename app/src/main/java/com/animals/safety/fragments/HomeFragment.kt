f5641e298b844cbbec100f3966a961cd8cbff54a
ludovicroland <ludovic.roland@decathlon.com>
2024-02-10 00:15:06 +0100

    chore(kotlin): migrate the HomeFragment to kotlin

--- app/src/main/java/com/animals/safety/fragments/HomeFragment.kt
+++ app/src/main/java/com/animals/safety/fragments/HomeFragment.kt
@@ -1,68 +1,62 @@
-package com.animals.safety.fragments;
+package com.animals.safety.fragments
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.animals.safety.R
+import com.animals.safety.adapter.AnimalCardAdapter
+import com.animals.safety.data.AnimalData
+import com.animals.safety.databinding.FragmentHomeBinding
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import com.animals.safety.R;
-import com.animals.safety.adapter.AnimalCardAdapter;
-import com.animals.safety.adapter.OnAnimalClickListener;
-import com.animals.safety.data.Animal;
-import com.animals.safety.data.AnimalData;
-import com.animals.safety.databinding.FragmentHomeBinding;
-
-public class HomeFragment extends Fragment {
-
-    private FragmentHomeBinding binding;
-    private AnimalCardAdapter adapter;
-
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+class HomeFragment : Fragment() {
+  
+  private var binding: FragmentHomeBinding? = null
+  
+  private var animalCardAdapter: AnimalCardAdapter? = null
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    binding = FragmentHomeBinding.inflate(inflater, container, false)
+    return binding?.root
+  }
+  
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    
+    animalCardAdapter = AnimalCardAdapter(AnimalData.animals) { animal ->
+      val bundle = Bundle().apply {
+        putSerializable(DetailsFragment.KEY_ANIMAL, animal)
+      }
+      
+      NavHostFragment.findNavController(this@HomeFragment)
+        .navigate(R.id.action_HomeFragment_to_DetailsFragment, bundle)
     }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        adapter = new AnimalCardAdapter(AnimalData.animals, new OnAnimalClickListener() {
-            @Override
-            public void onClick(Animal animal) {
-                Bundle bundle = new Bundle();
-                bundle.putSerializable(DetailsFragment.KEY_ANIMAL, animal);
-
-                NavHostFragment.findNavController(HomeFragment.this)
-                        .navigate(R.id.action_HomeFragment_to_DetailsFragment, bundle);
-            }
-        });
-
-        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        binding.recyclerView.setAdapter(adapter);
-        binding.recyclerView.addItemDecoration(new DividerItemDecoration(binding.recyclerView.getContext(), DividerItemDecoration.VERTICAL));
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(HomeFragment.this)
-                        .navigate(R.id.action_HomeFragment_to_CreateFragment);
-            }
-        });
+    binding?.recyclerView?.apply {
+      layoutManager = LinearLayoutManager(context)
+      adapter = animalCardAdapter
+      addItemDecoration(
+        DividerItemDecoration(
+          context,
+          DividerItemDecoration.VERTICAL
+        )
+      )
     }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    
+    binding?.fab?.setOnClickListener {
+      NavHostFragment.findNavController(this@HomeFragment)
+        .navigate(R.id.action_HomeFragment_to_CreateFragment)
     }
-
+  }
+  
+  override fun onDestroyView() {
+    super.onDestroyView()
+    binding = null
+  }
+  
 }
