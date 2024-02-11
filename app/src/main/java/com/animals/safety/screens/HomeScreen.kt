010bcda058ecd70747c05b01955ff7725eedb92b
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-02-11 22:46:17 +0100

    chore(compose): rename compose function into the HomeScreen

--- app/src/main/java/com/animals/safety/screens/HomeScreen.kt
+++ app/src/main/java/com/animals/safety/screens/HomeScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +34,7 @@ import java.util.UUID
 fun HomeScreen(
   modifier: Modifier = Modifier,
   animals: List<Animal> = AnimalData.animals,
-  onAnimalClick: (Animal?) -> Unit = {},
+  onAnimalClick: (Animal) -> Unit = {},
 ) {
   Scaffold(
     modifier = modifier,
@@ -60,7 +59,7 @@ fun HomeScreen(
       }
     }
   ) { contentPadding ->
-    HomeListScreen(
+    HomeList(
       modifier = modifier.padding(contentPadding),
       animals = animals,
       onAnimalClick = onAnimalClick
@@ -69,14 +68,14 @@ fun HomeScreen(
 }
 
 @Composable
-private fun HomeListScreen(
+private fun HomeList(
   modifier: Modifier = Modifier,
   animals: List<Animal>,
-  onAnimalClick: (Animal?) -> Unit,
+  onAnimalClick: (Animal) -> Unit,
 ) {
   LazyColumn(modifier) {
     items(animals) { animal ->
-      HomeCellScreen(
+      HomeCell(
         animal = animal,
         onAnimalClick = onAnimalClick
       )
@@ -86,9 +85,9 @@ private fun HomeListScreen(
 }
 
 @Composable
-private fun HomeCellScreen(
+private fun HomeCell(
   animal: Animal,
-  onAnimalClick: (Animal?) -> Unit,
+  onAnimalClick: (Animal) -> Unit,
 ) {
   //TODO
   Box(
@@ -96,16 +95,16 @@ private fun HomeCellScreen(
       .fillMaxWidth()
       .height(20.dp)
       .clickable {
-        onAnimalClick
+        onAnimalClick(animal)
       }
   )
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun HomeCellScreenPreview() {
+private fun HomeCellPreview() {
   AimantsDanimauxTheme(dynamicColor = false) {
-    HomeCellScreen(
+    HomeCell(
       animal = Animal(UUID.randomUUID(),"Milou", Breed.DOG, 6, 23.2f, 42.4f),
       onAnimalClick = {}
     )
