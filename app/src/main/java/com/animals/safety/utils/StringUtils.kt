441d704de2f06bf4ae9d32496a341a71c04e938b
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-02-12 23:04:56 +0100

    chore(kotlin): remove the StringUtils file

--- app/src/main/java/com/animals/safety/utils/StringUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.animals.safety.utils
-
-fun String.capitalize(input: String?): String {
-  val sb = StringBuilder(input)
-  sb.setCharAt(0, sb[0].uppercaseChar())
-  return sb.toString()
-}
