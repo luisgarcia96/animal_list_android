f0d663ac27734cf05c9913f8e6df1994b275b584
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-02-12 23:57:25 +0100

    chore(compose): clean-up the code

--- app/src/main/java/com/animals/safety/screens/CreateAnimalScreen.kt
+++ app/src/main/java/com/animals/safety/screens/CreateAnimalScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.animals.safety.R
 import com.animals.safety.data.Animal
 import com.animals.safety.data.AnimalData
@@ -164,6 +165,7 @@ private fun CreateAnimal(modifier: Modifier = Modifier) {
 
   Column(
     modifier = modifier
+      .padding(bottom = 88.dp, top = 16.dp, start = 16.dp, end = 16.dp)
       .fillMaxSize()
       .verticalScroll(scrollState)
   ) {
